61dba6826c35b08506bb3a1c40e0e485ca77acf0
Sean Fessenden <sean.fessenden@gmail.com>
2026-06-14 09:18:55 -0400

    Added lesson 15 homework and changes from my_camera.py

--- ai_on_the_edge/my_camera.py
+++ ai_on_the_edge/my_camera.py
@@ -1,5 +1,7 @@
 import cv2
 from picamera2 import Picamera2
+from time import sleep
+import threading as th
 
 class MyCamera:
     picture = "Picture"
@@ -10,6 +12,7 @@ class MyCamera:
         self.WIDTH = width
         self.HEIGHT = height
         RES = (width, height)
+        self.running = True
         self.piCam.preview_configuration.main.size = RES
         self.piCam.preview_configuration.main.format = 'RGB888'
         self.piCam.preview_configuration.align()
@@ -24,14 +27,23 @@ class MyCamera:
             cv2.destroyAllWindows()
             
     def capture_video(self):
-        while True:
+        while self.running:
             frame = self.piCam.capture_array()
             cv2.imshow(MyCamera.video, frame)
             cv2.moveWindow(MyCamera.video, 0, 60)
             if cv2.waitKey(1) == ord('q'): #need the '1' so that the loop will work
-                cv2.destroyAllWindows()
+                break
+        cv2.destroyAllWindows()
+    #end capture_video
+        
+    def stop(self):
+        self.running = False
         
 if __name__ == "__main__":
     myCam = MyCamera(1280, 720, "Test Picture")
-    myCam.capture_picture()
-    myCam.capture_video()
+
+    t = th.Thread(target=myCam.capture_video)
+    t.start()
+    sleep(3)
+    myCam.stop()
+    t.join()
