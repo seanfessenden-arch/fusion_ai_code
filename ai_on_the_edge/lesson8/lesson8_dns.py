1236b75afa8b5a00e740b1690d210758993fa3df
Sean Fessenden <sean.fessenden@gmail.com>
2026-06-16 17:55:16 -0500

    Fix nested ai_on_the_edge folder structure

--- ai_on_the_edge/lesson8/lesson8_dns.py
+++
@@ -1,4 +0,0 @@
-import socket
-
-addr = socket.getaddrinfo("huggingface.co", 443, socket.AF_INET)
-print(addr[0][4][0])
--- ai_on_the_edge/lesson8/lesson8_voices.py
+++
@@ -1,6 +0,0 @@
-from fusion_hat.tts import Piper
-
-tts = Piper()
-tts.set_model('en_US-amy-low')
-msg = "Test this voice"
-tts.say(msg, stream=False)
